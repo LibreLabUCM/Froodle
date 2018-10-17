3c5888016c85628544186e5684d78a86b4da9768
davas-web-designs <36100125+davas-web-designs@users.noreply.github.com>
2018-10-17 10:43:41 +0200

    Added db schema
    Added the schema for the db structure

--- db.php
+++ db.php
@@ -1,4 +1,17 @@
 require ('vendor/autoload.php');
 
+/*
+  DB STRUCTURE:
+  
+  DOODLE
+    ⊢->TITLE
+    ⊢-> FECHAS
+    ︳      ﹂DAY
+    ︳         ﹂HOURS
+    ﹂USERS
+        ﹂USERNAME
+              ﹂OPTIONS(Array containing 0,1 or 2 as much times as options there are)
+*/
+
 $cliente = new MongoDB\Client("mongodb://localhost:27017");
 $colección = $cliente->froodle->users;
