243418023ae12e438b4e97d86b3a8dae03ca5b28
davas-web-designs <36100125+davas-web-designs@users.noreply.github.com>
2018-10-20 13:59:51 +0200

    added validate.php
    this file sends the info to the db

---
+++ validate.php
@@ -0,0 +1,18 @@
+<?php
+    require ('vendor/autoload.php');
+
+    $cliente = new MongoDB\Client("mongodb://localhost:27017");
+    $coleccion = $cliente->froodle->test;
+
+    $val = $_POST['value'];
+    $title = $val[0];
+
+    $array = array();
+    for($i = 0; $i < count($val) - 1; $i++){
+      $arr = $val[$i + 1];
+      array_push($array, array('date'=> $arr['date'], 'time'=> $arr['time']));
+    }
+
+    print_r($array);
+    $coleccion->insertOne(["title" => "froodle13", "dates" => $array]);
+?>
