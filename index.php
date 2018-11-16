a0c77d7a877afd48b0711a361d4ffaf6455b9dda
DevPGSV <DevPGSV@gmail.com>
2018-11-16 23:14:42 +0100

    Changed extension from html to php (so php code is executed)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require ('db.php'); ?>
 <!--this will allow us to call all the php code from this page if needed-->
 <!DOCTYPE html>
-<html >
+<html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
     <title>Welcome to froodle</title>
@@ -21,12 +21,12 @@
       <source src="http://mazwai.com/system/posts/videos/000/000/109/webm/leif_eliasson--glaciartopp.webm?random=1" type="video/webm">
     </video>
     <div class="box">
-	
+
       <h1>Froodle</h1>
-	 
+
       <center><h2 id="demo">9999 </h2></center>
       <center><h3>froodles created</h3></center>
-   
+
 
       <button onclick="location.href='https://librelabucm.org/create_froodle.php'">Create Froodle</button>
       <p>What is froodle? <span onclick="location.href='https://librelabucm.org/'">Follow the Froodle team</span></p>
