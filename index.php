16a7b2b31deafe447656ec64a956aa0884cb1653
davas-web-designs <36100125+davas-web-designs@users.noreply.github.com>
2018-11-18 19:04:04 +0100

    Did a real index.php

--- index.php
+++ index.php
@@ -1,11 +1,46 @@
 <?php require ('db.php'); ?>
-
+<!--this will allow us to call all the php code from this page if needed-->
+<!DOCTYPE html>
 <html>
- <head>
-  <title>Froodle</title>
- </head>
- <body>
-    <p>Hola mundo!</p>
-    <?php echo "<p>Hello World</p>" ?>
- </body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Welcome to froodle</title>
+    <!--Link Css Sheets-->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+  <div class="inner-container">
+
+    <div class="box">
+
+      <h1>Froodle</h1>
+
+    <h2 id="demo">
+
+      <?php
+
+        $result = $coleccion -> find();
+        $counter = 0;
+        foreach($result as $entry){
+          $counter++;
+        }
+        echo $counter;
+
+
+      ?>
+
+    </h2>
+    <h3>froodles created</h3>
+
+
+      <button onclick="location.href='modules/createFroodle.html'">Create Froodle</button>
+      <p>What is froodle? <span onclick="location.href='https://librelabucm.org/'">Follow the Froodle team</span></p>
+      <p>Powered by librelab ucm</p>
+    </div>
+  </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  </body>
 </html>
