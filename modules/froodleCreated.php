736c18bef41084f3d419b135fcd2fda70da555a7
davas-web-designs <36100125+davas-web-designs@users.noreply.github.com>
2018-11-18 19:02:06 +0100

    added styles

--- modules/froodleCreated.php
+++ modules/froodleCreated.php
@@ -5,45 +5,49 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-    <form class="" action="options.php" method="post">
+    <div class="inner-container">
+      <form class="" action="options.php" method="post">
 
-      <span>Username: </span>
-      <input type="text" name = "username" value="">
-      <br>
-      <br>
+        <span>Username: </span>
+        <input type="text" name = "username" value="">
+        <br>
+        <br>
 
-      <?php
+        <?php
 
-        $title = $_GET['title'];
+          $title = $_GET['title'];
 
-        echo "froodle title: ". $title . '<br><br>';
-        echo "options: " . "<br>";
+          echo "froodle title: ". $title . '<br><br>';
+          echo "options: " . "<br>";
 
 
-        $result = $coleccion -> findOne(array('title' => $title));
-        $counter = 0;
-        foreach($result['dates'] as $date){
-          echo "dia: ". $date['date'];
-          echo "<ul>";
-          foreach ($date['time'] as $time) {
-            echo "<li>  hora: " . $time['hour'] ;
-            echo "<input type = 'radio' name = 'option".$counter."' value = 'yes'>YES";
-            echo "<input type = 'radio' name = 'option".$counter."' value = 'no'>NO";
-            echo "</li>";
-            $counter++;
+          $result = $coleccion -> findOne(array('title' => $title));
+          $counter = 0;
+          foreach($result['dates'] as $date){
+            echo "dia: ". $date['date'];
+            echo "<ul>";
+            foreach ($date['time'] as $time) {
+              echo "<li>  hora: " . $time['hour'] ;
+              echo "<input type = 'radio' name = 'option".$counter."' value = 'yes'>YES";
+              echo "<input type = 'radio' name = 'option".$counter."' value = 'no'>NO";
+              echo "</li>";
+              $counter++;
+            }
+            echo "</ul>";
+            echo "<input type = 'hidden' name = 'qty' value = ".$counter.">";
+            echo "<input type = 'hidden' name = 'title' value = ".$title.">";
           }
-          echo "</ul>";
-          echo "<input type = 'hidden' name = 'qty' value = ".$counter.">";
-          echo "<input type = 'hidden' name = 'title' value = ".$title.">";
-        }
-      ?>
-      <br>
-      <br>
-      <button type="submit" name="button" id = "submit">Submit options</button>
+        ?>
+        <br>
+        <br>
+        <button type="submit" name="button" id = "submit">Submit options</button>
 
 
-    </form>
+      </form>
+
+    </div>
   </body>
 </html>
