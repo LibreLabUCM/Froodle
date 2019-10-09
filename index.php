5b2fd005190d51ccfd471c55fe423db49e59ffd2
Pedro Javier <pedro-javierf@users.noreply.github.com>
2019-10-09 22:25:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
+<!--
+    Froodle index.php
+    Copyright (C) 2019 LibreLabUCM
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
 <?php require ('db.php'); ?>
 <!--this will allow us to call all the php code from this page if needed-->
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="es" dir="ltr">
   <head>
     <meta charset="UTF-8">
     <title>Welcome to froodle</title>
@@ -16,10 +33,8 @@
   <div class="inner-container">
 
     <div class="box">
-
       <h1>Froodle</h1>
-
-      <h2 id="demo">
+        <h2 id="demo">
 
         <?php
 
@@ -33,14 +48,13 @@
         ?>
 
       </h2>
-      <h3>froodles created</h3>
-
+      <h3>Froodles Created</h3>
 
       <button onclick="location.href='modules/createFroodle.php'">Create Froodle</button>
       <p>or</p>
       <button onclick = "location.href = 'modules/viewFroodle.php'" type="button" name="button">View your froodle</button>
       <p>What is froodle? <span onclick="location.href='https://librelabucm.org/'">Follow the Froodle team</span></p>
-      <p>Powered by librelab ucm</p>
+      <p>Powered by LibreLabUcm</p>
     </div>
   </div>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
